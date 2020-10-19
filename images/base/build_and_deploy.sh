d7d1981bd109473b622fbbb4e7d62f46cb9af906
Phillipp Drieger <pdrieger@splunk.com>
2020-10-19 10:36:18 +0200

    dltk image: update for 4.0 base image

--- images/base/build_and_deploy.sh
+++ images/base/build_and_deploy.sh
@@ -3,5 +3,5 @@ set -e
 set -x
 
 ./build.sh golden-image-gpu-4 phdrieger/ 4.0.0
-docker tag phdrieger/mltk-container-golden-image-gpu-4:4.0.0 hovu96/dltk-golden-image-gpu:4.0.0
-docker push hovu96/dltk-golden-image-gpu:4.0.0
+docker tag phdrieger/mltk-container-golden-image-gpu-4:4.0.0 phdrieger/dltk-base-golden-image-gpu:4.0.0
+docker push phdrieger/dltk-base-golden-image-gpu:4.0.0

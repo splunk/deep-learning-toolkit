6324c6b7b85427c52f206f4d3b98118e75d687bb
hovu96 <rfujara@splunk.com>
2020-10-25 19:58:15 +0000

    tracing for #6: remove socket SOCK_DGRAM stuff

--- app/bin/jaeger_client/utils.py
+++ app/bin/jaeger_client/utils.py
@@ -66,58 +66,11 @@ def local_ip():
         ip = socket.gethostbyname(socket.gethostname())
     except IOError:
         ip = socket.gethostbyname('localhost')
-    if ip.startswith('127.'):
-        ip = get_local_ip_by_interfaces()
-        if ip is None:
-            ip = get_local_ip_by_socket()
+    # if ip.startswith('127.'):
+    #    ip = get_local_ip_by_interfaces()
+    #    if ip is None:
+    #        ip = get_local_ip_by_socket()
     return ip
-
-
-def get_local_ip_by_socket():
-    # Explanation : https://stackoverflow.com/questions/166506
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.255.255.255', 1))
-        ip = s.getsockname()[0]
-    except IOError:
-        ip = None
-    finally:
-        s.close()
-    return ip
-
-
-def get_local_ip_by_interfaces():
-    ip = None
-    # Check eth0, eth1, eth2, en0, ...
-    interfaces = [
-        i + bytes(n) for i in (b'eth', b'en', b'wlan') for n in range(3)
-    ]  # :(
-    for interface in interfaces:
-        try:
-            ip = interface_ip(interface)
-            if ip is not None:
-                break
-        except IOError:
-            pass
-    return ip
-
-
-def interface_ip(interface):
-    try:
-        import fcntl
-        """Determine the IP assigned to us by the given network interface."""
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        return socket.inet_ntoa(
-            fcntl.ioctl(
-                sock.fileno(), 0x8915, struct.pack('256s', interface[:15])
-            )[20:24]
-        )
-    except ImportError:
-        return None
-    # Explanation:
-    # http://stackoverflow.com/questions/11735821/python-get-localhost-ip
-    # http://stackoverflow.com/questions/24196932/how-can-i-get-the-ip-address-of-eth0-in-python
 
 
 def raise_with_value(exception, value):

79e3a1b658c3faa6736e54886b4f0c2552a35ec5
hovu96 <rfujara@splunk.com>
2020-11-06 09:50:11 +0000

    fix path path to lib/

--- app/bin/rest_handlers.py
+++ app/bin/rest_handlers.py
@@ -4,7 +4,7 @@ import sys
 bin_path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 if bin_path not in sys.path:
     sys.path.insert(0, bin_path)
-lib_path = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__))))
+lib_path = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), "lib"))
 if lib_path not in sys.path:
     sys.path.insert(0, lib_path)
 
